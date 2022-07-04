f8f88eb3217b2856fdc3b619c96d5e4e6d48d110
brunotakazono <brunomatsuyama@hotmail.com>
2022-07-04 15:15:37 -0300

    Update test_b_bst.py

--- balanced_binary_search_tree/test_b_bst.py
+++ balanced_binary_search_tree/test_b_bst.py
@@ -4,26 +4,13 @@ from b_bst import *
 
 s_arr = [1, 4, 9, 16, 25, 36]
 root = sorted_arr_balancedBST(s_arr)
-print ('Preorder Traversal of constructed BST ')
+print ('Preorder Balanced BST ')
 preorder(root)
 print('\n')
 print('-' * 50)
-print ('Inorder Traversal of constructed BST ')
+print ('Inorder Balance BST ')
 in_order(root)
 print('\n')
 print('-' * 50)
-print ('Posorder Traversal of constructed BST ')
+print ('Posorder Balanced BST ')
 pos_order(root)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
