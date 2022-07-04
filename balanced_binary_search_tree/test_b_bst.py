76d252b22c134264b666ea1744e1a5d168781051
brunotakazono <brunomatsuyama@hotmail.com>
2022-07-04 15:28:33 -0300

    Add files via upload

---
+++ balanced_binary_search_tree/test_b_bst.py
@@ -0,0 +1,29 @@
+from b_bst import *
+
+
+
+s_arr = [1, 4, 9, 16, 25, 36]
+root = sorted_arr_balancedBST(s_arr)
+print ('Preorder Traversal of constructed BST ')
+preorder(root)
+print('\n')
+print('-' * 50)
+print ('Inorder Traversal of constructed BST ')
+in_order(root)
+print('\n')
+print('-' * 50)
+print ('Posorder Traversal of constructed BST ')
+pos_order(root)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
